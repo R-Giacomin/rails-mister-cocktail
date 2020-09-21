4997bef6c3a72bd007d43e0681ae68819d8de369
Rafael Giacomin <faellg@gmail.com>
2020-09-21 15:39:07 -0300

    rails-mister-cocktail - lecombe

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :create, :new, :show] do
+    resources :doses, only: [:create, :new]
+  end
+  resources :doses, only: [:destroy]
+
+   root to: 'cocktails#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create!(name: "Limão")
+Ingredient.create!(name: "Gelo")
+Ingredient.create!(name: "Menta")
+Ingredient.create!(name: "Suco de laranja")
+Ingredient.create!(name: "Coca cola")
+Ingredient.create!(name: "Vodka")
+Ingredient.create!(name: "Cachaça")
+Ingredient.create!(name: "Tequila")
+Ingredient.create!(name: "Açucar")
+Ingredient.create!(name: "Hortelã")
+Ingredient.create!(name: "Maracujá")
+Ingredient.create!(name: "Água com gás")
